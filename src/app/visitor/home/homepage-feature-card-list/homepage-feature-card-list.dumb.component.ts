5ff3ff811c59776043b69965b6243b286e037256
Dieny Simon <simon.dieny@gmail.com>
2025-01-20 13:26:09 +0100

    wip(home): generate dumb component

---
+++ src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.dumb.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-feature-card-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage-feature-card-list.dumb.component.html',
+  styleUrl: './homepage-feature-card-list.dumb.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomepageFeatureCardListDumbComponent {
+
+}
