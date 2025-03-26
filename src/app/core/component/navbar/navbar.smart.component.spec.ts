f497a01799694cdf4ea07374a68ab571bbbd2b0e
Dieny Simon <simon.dieny@gmail.com>
2025-03-26 17:49:46 +0000

    [User story #2/Sprint #1] Create an account as a new user (#12)

--- src/app/core/component/navbar/navbar.smart.component.spec.ts
+++ src/app/core/component/navbar/navbar.smart.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarSmartComponent } from './navbar.smart.component';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
 describe('NavbarSmartComponent', () => {
   let component: NavbarSmartComponent;
@@ -13,6 +13,7 @@ describe('NavbarSmartComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavbarSmartComponent],
+      providers: [provideRouter([]),]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarSmartComponent);
---
+++ src/app/visitor/signup/signup.page.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserStore } from '../../core/store/user.store';
+import { AuthenticationService } from '../../core/port/authentication.service';
+import { Visitor } from '../../core/entity/user.interface';
+
+@Component({
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './signup.page.component.html',
+  styleUrl: './signup.page.component.scss',
+})
+export class SignupPageComponent {
+  readonly store = inject(UserStore);
+  readonly authenticationService = inject(AuthenticationService);
+
+  readonly name = signal('');
+  readonly email = signal('');
+  readonly password = signal('');
+  readonly confirmPassword = signal('');
+
+  readonly isPasswordMatch = computed(
+    () => this.password() === this.confirmPassword()
+  );
+
+  onSubmit() {
+    const visitor: Visitor = {
+      name: this.name(),
+      email: this.email(),
+      password: this.password(),
+    };
+    this.store.register(visitor);
+  }
+}
