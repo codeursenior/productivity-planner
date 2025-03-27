51f4b0cced532d5645469b1c6d1d49d031052f3f
Dieny Simon <simon.dieny@gmail.com>
2025-03-27 19:22:16 +0000

    [Task-11] Create page component Login (#14)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './visitor/home/home.page.component';
 import { SignupPageComponent } from './visitor/signup/signup.page.component';
+import { LoginPageComponent } from './visitor/login/login.page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
     title: 'Productivity Planner',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    title: 'Login'
   },
   {
     path: 'signup',
