86ecdde7083a0f07481d1a8ab761121de4df7b3e
Dieny Simon <simon.dieny@gmail.com>
2025-04-14 11:13:45 +0200

    task(store): export signal store type

--- src/app/core/store/user.store.ts
+++ src/app/core/store/user.store.ts
@@ -11,7 +11,7 @@ import { User } from '../entity/user.interface';
 interface UserState {
   user: User | undefined;
 }
-
+export type UserStore = InstanceType<typeof UserStore>;
 export const UserStore = signalStore(
   { providedIn: 'root' },
   withState<UserState>({
