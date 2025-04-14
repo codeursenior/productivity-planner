5dfeda5843cc31e6d16f78987abf0b08d49ef78e
Dieny Simon <simon.dieny@gmail.com>
2025-04-14 11:19:51 +0200

    [TASK-15] Auto connect existing user (#19)

--- src/app/core/adapter/authentication-firebase.service.ts
+++ src/app/core/adapter/authentication-firebase.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@env/environment';
 import { catchError, map, Observable, of, throwError } from 'rxjs';
@@ -15,7 +15,7 @@ import { InvalidCredentialError } from '@app/visitor/login/domain/invalid-creden
  *
  * @see https://firebase.google.com/docs/reference/rest/auth?hl=fr#section-create-email-password
  */
-interface FirebaseResponseSignup {
+interface FirebaseResponseSignupPayload {
   idToken: string;
   email: string;
   refreshToken: string;
@@ -23,7 +23,7 @@ interface FirebaseResponseSignup {
   localId: string;
 }
 
-interface FirebaseResponseSignin {
+interface FirebaseResponseSigninPayload {
   displayName: string;
   email: string;
   expiresIn: string;
@@ -33,6 +33,16 @@ interface FirebaseResponseSignin {
   registered: boolean;
 }
 
+export interface FirebaseRefreshTokenPayload {
+  expires_in: string;
+  token_type: string; // always "Bearer"
+  refresh_token: string;
+  id_token: string;
+  user_id: string;
+  project_id: string;
+}
+
+
 @Injectable()
 export class AuthenticationFirebaseService implements AuthenticationService {
   readonly #http = inject(HttpClient);
@@ -41,12 +51,12 @@ export class AuthenticationFirebaseService implements AuthenticationService {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseConfig.apiKey}`;
     const body = { email, password, returnSecureToken: true };
 
-    return this.#http.post<FirebaseResponseSignup>(url, body).pipe(
+    return this.#http.post<FirebaseResponseSignupPayload>(url, body).pipe(
       map((response) => ({
-          jwtToken: response.idToken,
-          jwtRefreshToken: response.refreshToken,
-          expiresIn: response.expiresIn,
-          userId: response.localId,
+        jwtToken: response.idToken,
+        jwtRefreshToken: response.refreshToken,
+        expiresIn: response.expiresIn,
+        userId: response.localId,
       })),
       catchError(error => {
         if(error.error.error.message === 'EMAIL_EXISTS') {
@@ -62,7 +72,7 @@ export class AuthenticationFirebaseService implements AuthenticationService {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseConfig.apiKey}`;
     const body = { email, password, returnSecureToken: true };
 
-    return this.#http.post<FirebaseResponseSignin>(url, body).pipe(
+    return this.#http.post<FirebaseResponseSigninPayload>(url, body).pipe(
       map((response) => ({
         jwtToken: response.idToken,
         jwtRefreshToken: response.refreshToken,
@@ -77,6 +87,20 @@ export class AuthenticationFirebaseService implements AuthenticationService {
 
         return throwError(() => error);
       })
+    );
+  }
+
+  refreshToken(refreshToken: string): Observable<{ jwtToken: string, userId: string }> {
+    const url = `https://securetoken.googleapis.com/v1/token?key=${environment.firebaseConfig.apiKey}`;
+    const body = new HttpParams()
+      .set('grant_type', 'refresh_token')
+      .set('refresh_token', refreshToken)
+      .toString();
+  
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+  
+    return this.#http.post<FirebaseRefreshTokenPayload>(url, body, { headers }).pipe(
+      map(response => ({ jwtToken: response.id_token, userId: response.user_id })),
     );
   }
 }
