1857430fb301b550ba89186e6f824ef4b1fcc9a7
Dieny Simon <simon.dieny@gmail.com>
2025-06-12 13:06:52 +0200

    wip(workday): provide empty signal store

--- src/app/membership/workday/workday.page.component.ts
+++ src/app/membership/workday/workday.page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BookSearchStore } from './workday.page.store';
 
 @Component({
   imports: [],
   templateUrl: './workday.page.component.html',
-  styleUrl: './workday.page.component.scss'
+  styleUrl: './workday.page.component.scss',
+  providers: [BookSearchStore]
+
 })
 export class WorkdayPageComponent {
 
