805bc82f4f7298bbac88a7929d67e37c0d0163b7
Dieny Simon <simon.dieny@gmail.com>
2025-09-25 13:33:13 +0200

    [TASK-25] Create workday page wiew model (#24)

--- src/app/membership/settings/settings.page.component.ts
+++ src/app/membership/settings/settings.page.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  selector: 'app-settings-page',
   imports: [],
   templateUrl: './settings.page.component.html',
   styleUrl: './settings.page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsPageComponent {
-
-}
+export class SettingsPageComponent {}
